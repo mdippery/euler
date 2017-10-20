9e92b51b536f6fc677d004aa6ea25f11180648cb
Michael Dippery <michael@monkey-robot.com>
2017-10-20 16:05:15 -0700

    Solve Euler Problem #24

--- src/Euler.hs
+++ src/Euler.hs
@@ -1,5 +1,8 @@
 module Euler where
 
+import Data.List (permutations, sort)
 import Euler.Math (binomialCoefficient)
 
 problem15 = binomialCoefficient 20
+
+problem24 = head $ drop 999999 $ sort $ permutations "0123456789"
