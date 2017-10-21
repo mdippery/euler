bda84c082ccb078fe0f0053a41038262e360bbee
Michael Dippery <michael@monkey-robot.com>
2017-10-20 17:11:23 -0700

    Favor function composition over $ operator

--- src/Euler.hs
+++ src/Euler.hs
@@ -5,4 +5,4 @@ import Euler.Math (binomialCoefficient)
 
 problem15 = binomialCoefficient 20
 
-problem24 = head $ drop 999999 $ sort $ permutations "0123456789"
+problem24 = head $ drop 999999 $ (sort . permutations) "0123456789"
