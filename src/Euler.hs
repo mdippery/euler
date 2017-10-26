c16f420235392206ff978115e32af9206fdbcca5
Michael Dippery <michael@monkey-robot.com>
2017-10-26 10:28:31 -0700

    Add tests for all problems with known answers
    Over the years, I have solved several Project Euler problems, but in
    many cases, I no longer have the solutions. However, since I solved the
    problem, I do have the answers unlocked, so I added tests for these
    answers and plan to solve them all "canonically" in Haskell at some
    point.

--- src/Euler.hs
+++ src/Euler.hs
@@ -4,12 +4,60 @@ import Data.List (permutations, sort)
 import Euler.Math (binomialCoefficient, sumOfFactorials, sumOfPowers)
 import Euler.Util (pairHasEqualElements, pairWithFunc)
 
+notSolved = 0
+
+problem1 = notSolved
+
+problem2 = notSolved
+
+problem3 = notSolved
+
+problem4 = notSolved
+
+problem5 = notSolved
+
+problem6 = notSolved
+
+problem7 = notSolved
+
+problem8 = notSolved
+
+problem9 = notSolved
+
+problem10 = notSolved
+
+problem11 = notSolved
+
+problem12 = notSolved
+
+problem13 = notSolved
+
+problem14 = notSolved
+
 problem15 = binomialCoefficient 20
+
+problem16 = notSolved
+
+problem17 = notSolved
+
+problem18 = notSolved
+
+problem19 = notSolved
+
+problem20 = notSolved
+
+problem21 = notSolved
+
+problem22 = notSolved
 
 problem24 =
     let ps        = sort . permutations
         millionth = drop 999999
-    in  (head . millionth . ps) "0123456789"
+     in (head . millionth . ps) "0123456789"
+
+problem25 = notSolved
+
+problem29 = notSolved
 
 problem30 =
     let nums      = [2..355000]
@@ -23,3 +71,15 @@ problem34 =
         equalFactorials = filter pairHasEqualElements
         extract         = map fst
      in sum $ extract $ equalFactorials $ map (pairWithFunc sumOfFactorials) nums
+
+problem35 = notSolved
+
+problem36 = notSolved
+
+problem48 = notSolved
+
+problem53 = notSolved
+
+problem56 = notSolved
+
+problem67 = notSolved
--- tests/EulerTest.hs
+++ tests/EulerTest.hs
@@ -4,18 +4,134 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "Verify that problem 15 is correct" $ do
+  describe "Verify that problem 1" $ do
+    it "is correct" $ do
+      problem1 `shouldBe` 233168
+
+  describe "Verify that problem 2" $ do
+    it "is correct" $ do
+      problem2 `shouldBe` 4613732
+
+  describe "Verify that problem 3" $ do
+    it "is correct" $ do
+      problem3 `shouldBe` 6857
+
+  describe "Verify that problem 4" $ do
+    it "is correct" $ do
+      problem4 `shouldBe` 906609
+
+  describe "Verify that problem 5" $ do
+    it "is correct" $ do
+      problem5 `shouldBe` 232792560
+
+  describe "Verify that problem 6" $ do
+    it "is correct" $ do
+      problem6 `shouldBe` 25164150
+
+  describe "Verify that problem 7" $ do
+    it "is correct" $ do
+      problem7 `shouldBe` 104743
+
+  describe "Verify that problem 8" $ do
+    it "is correct" $ do
+      problem8 `shouldBe` 23514624000
+
+  describe "Verify that problem 9" $ do
+    it "is correct" $ do
+      problem9 `shouldBe` 31875000
+
+  describe "Verify that problem 10" $ do
+    it "is correct" $ do
+      problem10 `shouldBe` 142913828922
+
+  describe "Verify that problem 11" $ do
+    it "is correct" $ do
+      problem11 `shouldBe` 70600674
+
+  describe "Verify that problem 12" $ do
+    it "is correct" $ do
+      problem12 `shouldBe` 76576500
+
+  describe "Verify that problem 13" $ do
+    it "is correct" $ do
+      problem13 `shouldBe` 5537376230
+
+  describe "Verify that problem 14" $ do
+    it "is correct" $ do
+      problem14 `shouldBe` 837799
+
+  describe "Verify that problem 15" $ do
     it "is correct" $ do
       problem15 `shouldBe` 137846528820
 
-  describe "Verify that problem 24 is correct" $ do
+  describe "Verify that problem 16" $ do
+    it "is correct" $ do
+      problem16 `shouldBe` 1366
+
+  describe "Verify that problem 17" $ do
+    it "is correct" $ do
+      problem17 `shouldBe` 21124
+
+  describe "Verify that problem 18" $ do
+    it "is correct" $ do
+      problem18 `shouldBe` 1074
+
+  describe "Verify that problem 19" $ do
+    it "is correct" $ do
+      problem19 `shouldBe` 171
+
+  describe "Verify that problem 20" $ do
+    it "is correct" $ do
+      problem20 `shouldBe` 648
+
+  describe "Verify that problem 21" $ do
+    it "is correct" $ do
+      problem21 `shouldBe` 31626
+
+  describe "Verify that problem 22" $ do
+    it "is correct" $ do
+      problem22 `shouldBe` 871198282
+
+  describe "Verify that problem 24" $ do
     it "is correct" $ do
       problem24 `shouldBe` "2783915460"
 
-  describe "Verify that problem 30 is correct" $ do
+  describe "Verify that problem 25" $ do
+    it "is correct" $ do
+      problem25 `shouldBe` 4782
+
+  describe "Verify that problem 29" $ do
+    it "is correct" $ do
+      problem29 `shouldBe` 9183
+
+  describe "Verify that problem 30" $ do
     it "is correct" $ do
       problem30 `shouldBe` 443839
 
-  describe "Verify that problem 34 is correct" $ do
+  describe "Verify that problem 34" $ do
     it "is correct" $ do
       problem34 `shouldBe` 40730
+
+  describe "Verify that problem 35" $ do
+    it "is correct" $ do
+      problem35 `shouldBe` 55
+
+  describe "Verify that problem 36" $ do
+    it "is correct" $ do
+      problem36 `shouldBe` 872187
+
+  describe "Verify that problem 48" $ do
+    it "is correct" $ do
+      problem48 `shouldBe` 9110846700
+
+  describe "Verify that problem 53" $ do
+    it "is correct" $ do
+      problem53 `shouldBe` 4075
+
+  describe "Verify that problem 56" $ do
+    it "is correct" $ do
+      problem56 `shouldBe` 972
+
+  describe "Verify that problem 67" $ do
+    it "is correct" $ do
+      problem67 `shouldBe` 7273
