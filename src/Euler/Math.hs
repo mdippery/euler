e7d192d97723a1da90027fd804a796dc05ccf259
Michael Dippery <michael@monkey-robot.com>
2017-10-26 11:06:57 -0700

    Use point-free style for `sumDigits` function

--- src/Euler/Math.hs
+++ src/Euler/Math.hs
@@ -8,7 +8,7 @@ binomialCoefficient n = factorial (2 * n) `div` (factorial n * factorial n)
 
 mapDigits f = (map f) . digits
 
-sumDigits f = sum . f
+sumDigits = (.) sum
 
 digitPowers exp = mapDigits (^ exp)
 
