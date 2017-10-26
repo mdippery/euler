808c299af1d86914b1cedbbb3c7b85b52c9b34fe
Michael Dippery <michael@monkey-robot.com>
2017-10-26 11:13:55 -0700

    Provide type annotations for library functions

--- src/Euler/Math.hs
+++ src/Euler/Math.hs
@@ -2,19 +2,29 @@ module Euler.Math where
 
 import Euler.Text (digits)
 
+factorial :: Integer -> Integer
 factorial = product . (enumFromTo 1)
 
+divides :: Integer -> Integer -> Bool
+divides a b = b `rem` a == 0
+
+binomialCoefficient :: Integer -> Integer
 binomialCoefficient n = factorial (2 * n) `div` (factorial n * factorial n)
 
+mapDigits :: (Integer -> a) -> Integer -> [a]
 mapDigits f = (map f) . digits
 
+sumDigits :: (a -> [Integer]) -> a -> Integer
 sumDigits = (.) sum
 
+digitPowers :: Integral a => a -> Integer -> [Integer]
 digitPowers exp = mapDigits (^ exp)
 
+sumOfPowers :: Integral a => a -> Integer -> Integer
 sumOfPowers exp = sumDigits (digitPowers exp)
 
 digitFactorials :: Integer -> [Integer]
 digitFactorials = mapDigits factorial
 
+sumOfFactorials :: Integer -> Integer
 sumOfFactorials = sumDigits digitFactorials
--- src/Euler/Text.hs
+++ src/Euler/Text.hs
@@ -1,5 +1,7 @@
 module Euler.Text where
 
+toDigit :: Char -> Integer
 toDigit ch = read [ch]
 
+digits :: Integer -> [Integer]
 digits n = map toDigit (show n)
