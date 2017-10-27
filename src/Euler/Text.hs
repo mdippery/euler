18ebe3dd95ae495aab6f1381453cb6f81c88433f
Michael Dippery <michael@monkey-robot.com>
2017-10-26 17:40:02 -0700

    Calculate the "score" of a string
    ...as described in Euler Problem #22.

--- src/Euler/Text.hs
+++ src/Euler/Text.hs
@@ -1,7 +1,15 @@
 module Euler.Text where
 
+import Data.Char (ord)
+
 toDigit :: Char -> Integer
 toDigit ch = read [ch]
 
 digits :: Integer -> [Integer]
 digits n = map toDigit (show n)
+
+letterValue :: Char -> Int
+letterValue ch = ord ch - 64
+
+stringValue :: String -> Int
+stringValue = sum . map letterValue
