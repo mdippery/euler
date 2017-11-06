a61a130b9a19d31e8ed6394a221ba852ec82f40c
Michael Dippery <michael@monkey-robot.com>
2017-11-06 10:32:31 -0800

    License under the Unlicense
    This code isn't terribly important and is really just here for
    demonstration purposes. Copy it at will!

--- src/Euler/Data.hs
+++ src/Euler/Data.hs
@@ -1,3 +1,7 @@
+{- This is free and unencumbered software released into the public domain.
+   For the full text of the license, see the file LICENSE.
+   Additional licensing information may be found at http://unlicense.org. -}
+
 module Euler.Data where
 
 import qualified Data.Digits as D
--- tests/EulerTest.hs
+++ tests/EulerTest.hs
@@ -1,3 +1,7 @@
+{- This is free and unencumbered software released into the public domain.
+   For the full text of the license, see the file LICENSE.
+   Additional licensing information may be found at http://unlicense.org. -}
+
 import Euler
 import Euler.Grid
 import Euler.List
