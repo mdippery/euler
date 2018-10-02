349e47502853db5c0b0e363570a860e14df5c078
Michael Dippery <michael@monkey-robot.com>
2018-10-02 14:05:13 -0700

    Retrieve a given ring in a spiral of numbers
    Hopefully this will ease the solving of Euler Problem #58.

--- src/Euler/Spiral.hs
+++ src/Euler/Spiral.hs
@@ -11,7 +11,26 @@
   Calculates the sum of the diagonals of "spirals" of numbers. These "spirals"
   are defined in <https://projecteuler.net/problem=28 Euler Problem #28>.
 -}
-module Euler.Spiral where
+module Euler.Spiral
+  (
+    -- * Data types
+    Ring (..)
+  , ring
+
+    -- * Basic functions
+  , numRings
+  , sumDiagonals
+  ) where
+
+import Euler.Math (nextPrime)
+
+-- | Describes a single ring in a spiral of integers
+data Ring = Ring
+  { upperRight :: Integer   -- ^ Value of the upper right corner of the spiral
+  , upperLeft  :: Integer   -- ^ Value of the upper left corner of the spiral
+  , lowerLeft  :: Integer   -- ^ Value of the lower left corner of the spiral
+  , lowerRight :: Integer   -- ^ Value of the lower right corner of the spiral
+  } deriving Show
 
 -- | Calculates the sum of the diagonals of spiral (a series of /n/ rings).
 sumDiagonals :: Integer   -- ^ Number of rings in the spiral
@@ -28,3 +47,14 @@ sumDiagonals n =
 numRings :: Integer   -- ^ Size of length and width
          -> Integer   -- ^ Number of spirals
 numRings = flip div 2
+
+-- | Retrieves the /nth/ ring of a spiral.
+ring 0 = Ring 1 1 1 1
+ring n =
+  let delta = 8 * n
+      prev = ring (n - 1)
+      lr = delta + lowerRight prev
+      ll = (delta - 2) + lowerLeft prev
+      ul = (delta - 4) + upperLeft prev
+      ur = (delta - 6) + upperRight prev
+   in Ring ur ul ll lr
