df837039173787a2823ebed805284ad3748f89a0
Michael Dippery <michael@monkey-robot.com>
2018-10-17 12:40:51 -0700

    Revert complications to Euler.Spiral
    I don't think these will help in solving Euler Problem #58 after all,
    and they certainly make the module much more complicated to use.

--- src/Euler/Spiral.hs
+++ src/Euler/Spiral.hs
@@ -4,42 +4,18 @@
 
 {-|
   Module      : Euler.Spiral
-  Description : Calculates the sum of the diagonals of "spirals" of numbers
+  Description : Calculates properties of an Ulam spiral
   License     : UNLICENSE
   Maintainer  : michael@monkey-robot.com
 
-  Calculates the sum of the diagonals of "spirals" of numbers. These "spirals"
-  are defined in <https://projecteuler.net/problem=28 Euler Problem #28> and
-  <https://projecteuler.net/problem=58 Euler Problem #58>.
+  Calculates various properties of an <https://en.wikipedia.org/wiki/Ulam_spiral Ulam spiral>.
 -}
 module Euler.Spiral
   (
-    -- * Data types
-    Ring (..)
-  , primesInRing
-  , ring
-
-    -- * Basic functions
-  , diagonalSize
+    diagonalSize
   , numRings
-  , primeRatio
-  , primeRatioWithSideLength
-  , rings
-  , ringSize
   , sumDiagonals
   ) where
-
-import Data.List (genericLength)
-
-import Euler.Math (isPrime)
-
--- | Describes a single ring in a spiral of integers
-data Ring = Ring
-  { upperRight :: Integer   -- ^ Value of the upper right corner of the spiral
-  , upperLeft  :: Integer   -- ^ Value of the upper left corner of the spiral
-  , lowerLeft  :: Integer   -- ^ Value of the lower left corner of the spiral
-  , lowerRight :: Integer   -- ^ Value of the lower right corner of the spiral
-  } deriving Show
 
 -- | Calculates the sum of the diagonals of spiral (a series of /n/ rings).
 sumDiagonals :: Integer   -- ^ Number of rings in the spiral
@@ -53,49 +29,11 @@ sumDiagonals n =
    in ur + ul + ll + lr + sumDiagonals (n - 1)
 
 -- | Number of rings in a spiral with length and width of size /n/.
-numRings :: Integer   -- ^ Length and width of spiral
-         -> Integer   -- ^ Number of rings in the spiral
+numRings :: Integer   -- ^ Size of length and width
+         -> Integer   -- ^ Number of spirals
 numRings = flip div 2
-
--- | List of rings that make up the spiral with /n/ total rings.
-rings :: Integer -> [Ring]
-rings 0 = [ring 0]
-rings n = ring n : rings (n - 1)
-
--- | Retrieves the /nth/ ring of a spiral.
-ring 0 = Ring 1 1 1 1
-ring n =
-  let delta = 8 * n
-      prev = ring (n - 1)
-      lr = delta + lowerRight prev
-      ll = (delta - 2) + lowerLeft prev
-      ul = (delta - 4) + upperLeft prev
-      ur = (delta - 6) + upperRight prev
-   in Ring ur ul ll lr
-
--- | Number of elements in the /nth/ ring of a spiral of integers.
-ringSize :: Integer -> Integer
-ringSize = max 1 . (8 *)
 
 -- | Number of elements in the diagonal portions of a spiral with /n/ rings.
 diagonalSize :: Integer   -- ^ Number of rings in the spiral
-             -> Integer   -- ^ Total size of diagonals in the spiral
-diagonalSize = (+) 1 . (*) 4
-
--- | Number of primes in the ring.
-primesInRing :: Num a => Ring -> a
-primesInRing (Ring ur ul ll lr) = (genericLength . filter isPrime) [ur,ul,ll,lr]
-
--- | Calculates the ratio of prime numbers to all numbers in the diagonals of
--- a spiral with /n/ rings.
-primeRatio :: Fractional a
-           => Integer   -- ^ Total number of rings comprising the spiral
-           -> a         -- ^ Ratio of prime numbers to all numbers in the diagonal of the spiral
-primeRatio n = (fromIntegral . sum . map primesInRing . rings) n / (fromIntegral . diagonalSize) n
-
--- | Calculates the ratio of prime numbers to all numbers in the diagonals of
--- a spiral with sides of length /n/.
-primeRatioWithSideLength :: Fractional a
-                         => Integer   -- ^ Length of a side of the given spiral
-                         -> a         -- ^ Ratio of prime numbers to all numbers in the diagonal of the spiral
-primeRatioWithSideLength = primeRatio . numRings
+             -> Integer   -- ^ Number of elements in the diagonal of the spiral
+diagonalSize = (1 +) . (4 *)
